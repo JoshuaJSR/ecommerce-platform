ed7caae3553890ca53f3fb654ad3a44fda0a1cae
JoshuaJSR <joshjared2004@gmail.com>
2025-12-15 15:24:49 -0600

    feat: add class entity Usuario

---
+++ src/main/java/com/Joshua/backend/api/Model/Usuario.java
@@ -0,0 +1,101 @@
+package com.Joshua.backend.api.Model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @NotBlank
+    private String nombreUsuario;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @NotBlank
+    @JsonIgnore
+    @Size(min = 8)
+    private String password;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechaRegistro;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long idUsuario, String nombreUsuario, String correo, String password, LocalDate fechaRegistro) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.correo = correo;
+        this.password = password;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @PrePersist
+    void prePersist(){
+        this.fechaRegistro = LocalDate.now();
+    }
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "idUsuario=" + idUsuario +
+                ", nombreUsuario='" + nombreUsuario + '\'' +
+                ", correo='" + correo + '\'' +
+                ", fechaRegistro=" + fechaRegistro +
+                '}';
+    }
+}
+
